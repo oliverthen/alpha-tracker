ccb3ce1a08ded4a6948811a3bbc3520cb627502e
Oliver Then <oli171dr@gmail.com>
2023-11-30 18:38:55 -0500

    Password change, reset, and registration done

--- alphatracker/finance/views.py
+++ alphatracker/finance/views.py
@@ -2,28 +2,46 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
 
 def user_login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated sucessfully')
+                    return HttpResponse("Authenticated sucessfully")
                 else:
-                    return HttpResponse('Disabled account')
+                    return HttpResponse("Disabled account")
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse("Invalid login")
     else:
         form = LoginForm()
-    return render(request, 'finance/login.html', {'form': form})
+    return render(request, "finance/login.html", {"form": form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'finance/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "finance/register.html", {"user_form": user_form})
+
 
 @login_required
 def dashboard(request):
-    return render(request, 'finance/dashboard.html', {'section': 'dashboard'})
+    return render(request, "finance/dashboard.html", {"section": "dashboard"})
