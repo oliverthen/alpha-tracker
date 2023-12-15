db73d410adaedf045d66159fdcbe77bc97797214
Oliver Then <oli171dr@gmail.com>
2023-12-15 06:34:15 -0500

    Able to get ticker response

--- alphatracker/finance/management/commands/load_assets.py
+++ alphatracker/finance/management/commands/load_assets.py
@@ -14,13 +14,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # set up the API endpoint URL and parameters
-        endpoint = "http://api.marketstack.com/v1/exchanges"
+        endpoint = "http://api.marketstack.com/v1/exchanges/XNAS/tickers"
 
         # make the API request and get the JSON response
         response = requests.get(
             endpoint,
             params={
                 "access_key": settings.MARKETSTACK_API_KEY,
+                "limit": 1000,
             },
         )
         json_response = response.json()
+
+        # print(json_response)
+
+        # json_response["data"]
+        counter = 0
+
+        for data in json_response["data"]["tickers"]:
+            print(data["symbol"])
+            counter += 1
+        print(counter)
+
+        # for data in json_response["data"]:
+        #     asset = Asset.objects.get(ticker=data["symbol"])
+
+        #     try:
+        #         # check if price already exists
+        #         price = Price.objects.get(asset=asset)
+        #     except Price.DoesNotExist:
+        #         price = Price.objects.create(
+        #             asset=asset, price=data["adj_close"]
+        #         )
+
+        #         print(f"Price for {asset.ticker} on {day} created")
+        #     else:
+        #         print(f"Price for {asset.ticker} on {day} already exists")
